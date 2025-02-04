3b1092126390f065d611a2e8e587d3bd01718408
alexikicommit <alexis.jeanjean@ynov.com>
2025-02-04 17:00:46 +0100

    feat(front-authentication): Create login page

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core'
 import { TranslateModule, TranslatePipe } from '@ngx-translate/core'
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatButton } from '@angular/material/button'
+import { Router, RouterLink } from '@angular/router'
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,23 @@ import { TranslateModule, TranslatePipe } from '@ngx-translate/core'
   templateUrl: './login.component.html',
   imports: [
     TranslatePipe,
-    TranslateModule
+    TranslateModule,
+    FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatButton, RouterLink,
   ],
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  public value: string = "caca"
+  public loginForm: FormGroup
+
+  public constructor(public formBuilder: FormBuilder,
+                     public router: Router) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    })
+  }
+  public login() {
+    // TODO: Implement login
+  }
 }
