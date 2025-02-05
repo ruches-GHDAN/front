a1ebede8583e18ac4991601a19e7c60bf5cb8b74
alexikicommit <alexis.jeanjean@ynov.com>
2025-02-05 11:21:20 +0100

    feat(authentication): Create Register Page

--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButton } from '@angular/material/button'
+import { MatFormField, MatLabel } from '@angular/material/form-field'
+import { MatInput } from '@angular/material/input'
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+import { MatStep, MatStepLabel, MatStepper, MatStepperNext, MatStepperPrevious } from '@angular/material/stepper'
+import { TranslatePipe } from '@ngx-translate/core'
+import { RouterLink } from '@angular/router'
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    MatButton,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    ReactiveFormsModule,
+    MatStepper,
+    MatStep,
+    MatStepLabel,
+    MatStepperNext,
+    MatStepperPrevious,
+    TranslatePipe,
+    RouterLink,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  public firstFormGroup: FormGroup
+  public secondFormGroup: FormGroup
 
+  public constructor(private _formBuilder: FormBuilder) {
+   this.firstFormGroup = this._formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required]
+    })
+
+   this.secondFormGroup = this._formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  public register() {
+    //TODO: Implement register
+  }
 }
