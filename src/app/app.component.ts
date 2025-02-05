032716fba41b6a8ca678b42b6e2cbc7283491803
Guilhem <73174073+GuilhemTrd@users.noreply.github.com>
2025-02-05 15:30:14 +0100

    ✨ feat: mise en place de la navbar avec condition d'affichage (#24)
    * ✨ feat: mise en place de la navbar avec condition d'affichage
    
    * fix review MR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
 import { MultipleTransLoaderHttp } from './MultipleTransLoaderHttp';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  public selectedLanguage: string
+export class AppComponent implements OnInit {
+  public selectedLanguage: string;
+  public showNavbar = true;
 
   constructor(
     private translateService: MultipleTransLoaderHttp,
-    private translate: TranslateService
+    private translate: TranslateService,
+    private router: Router
   ) {
-    this.selectedLanguage = this.translateService.getLang() || 'fr'
-    this.translateService.setLang(this.selectedLanguage)
-    this.translate.use(this.selectedLanguage)
+    this.selectedLanguage = this.translateService.getLang() || 'fr';
+    this.translateService.setLang(this.selectedLanguage);
+    this.translate.use(this.selectedLanguage);
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.showNavbar = !['/login', '/register'].includes(event.urlAfterRedirects);
+      }
+    });
   }
 }
