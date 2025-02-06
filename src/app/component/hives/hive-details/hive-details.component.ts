bc6ce2232a388884c75381adc81352d43846f24e
alexikicommit <alexis.jeanjean@ynov.com>
2025-02-06 15:35:34 +0100

    feat(hive-details): create hive details page

---
+++ src/app/component/hives/hive-details/hive-details.component.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { MatButton } from '@angular/material/button'
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card'
+import { Hive, HiveHistory } from '../../../models/Hives.model'
+import { TranslatePipe } from '@ngx-translate/core'
+import { DatePipe } from '@angular/common'
+import { MatIcon } from '@angular/material/icon'
+
+@Component({
+  selector: 'app-hive-details',
+  imports: [
+    MatButton,
+    MatCard,
+    MatCardContent,
+    MatCardTitle,
+    MatCardHeader,
+    TranslatePipe,
+    DatePipe,
+    MatIcon,
+
+
+  ],
+  templateUrl: './hive-details.component.html',
+  styleUrl: './hive-details.component.scss'
+})
+export class HiveDetailsComponent {
+  public hiveId: string
+  public hive: Hive
+  public hiveHistory: HiveHistory[]
+
+  public constructor(private route: ActivatedRoute) {
+    this.hiveId = this.route.snapshot.paramMap.get('id')!
+    this.hive = {
+      id: parseInt(this.hiveId),
+      registration: 23485,
+      status: 'Active',
+      size: 10,
+      race: 'Italian',
+      temperature: 35,
+      queenYear: 2019,
+      longitude: 45.5,
+      latitude: 45.5
+    }
+
+    this.hiveHistory = [
+      {
+        date: '2021-09-02',
+        title: 'Hive creation',
+        description: 'Hive created with 10 bees',
+        idRuche: parseInt(this.hiveId)
+      },
+      {
+        date: '2021-09-03',
+        title: 'Hive update',
+        description: 'Hive updated with 5 bees',
+        idRuche: parseInt(this.hiveId)
+      },
+      {
+        date: '2021-09-04',
+        title: 'Hive update',
+        description: 'Hive updated with 15 bees',
+        idRuche: parseInt(this.hiveId)
+      }
+    ]
+  }
+}
