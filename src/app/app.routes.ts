b94f34a6aac6c2819dd57fe9b26aabe174c5846d
Hugo POIZAT <hugo.poizat@ynov.com>
2025-02-06 14:19:42 +0100

    Ajout page Dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router'
 import { LoginComponent } from './component/login/login.component'
 import { RegisterComponent } from './component/register/register.component'
+import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { HivesComponent } from './component/hives/hives.component'
 import { LandingComponent } from './component/landing/landing.component'
 
@@ -9,5 +10,9 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'hives', component: HivesComponent },
-  { path: 'landing', component: LandingComponent }
+  { path: 'landing', component: LandingComponent },
+
+  // Authenticated routes
+  // TODO : Ajouter AuthGuard pour que la route soit accessible uniquement si l'utilisateur est connecté
+  { path: 'dashboard', component: DashboardComponent }
 ];
