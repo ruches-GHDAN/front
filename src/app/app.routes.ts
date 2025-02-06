dc56bf929bea6c2b88ce6018319890927a1a7b7b
alexikicommit <alexis.jeanjean@ynov.com>
2025-02-06 15:34:32 +0100

    feat(errorPages): add basic config of error pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { LoginComponent } from './component/login/login.component'
 import { RegisterComponent } from './component/register/register.component'
 import { HivesComponent } from './component/hives/hives.component'
 import { LandingComponent } from './component/landing/landing.component'
+import { HiveDetailsComponent } from './component/hives/hive-details/hive-details.component'
+import { ErrorPageComponent } from './component/error-page/error-page.component'
 
 export const routes: Routes = [
   { path: '', redirectTo: 'landing', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'hives', component: HivesComponent },
-  { path: 'landing', component: LandingComponent }
-];
+  { path: 'landing', component: LandingComponent },
+  { path: 'hive/:id', component: HiveDetailsComponent },
+  { path: 'error/:errorKey', component: ErrorPageComponent },
+  { path: '**', redirectTo: 'error/404', pathMatch: 'full' }
+]
