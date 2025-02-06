b8e73fc517e73078cd673dfe07434d680a533701
guilhemtrd <guilhem.trd@gmail.com>
2025-02-06 11:35:11 +0100

    🔧✨ feat: création de la page Hives et ajustements de la navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
-import { MultipleTransLoaderHttp } from './MultipleTransLoaderHttp';
-import { NavbarComponent } from './component/navbar/navbar.component';
+import {Component, OnInit} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {NavigationEnd, Router} from '@angular/router';
+import {MultipleTransLoaderHttp} from './MultipleTransLoaderHttp';
+import {NavbarComponent} from './component/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [NavbarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
