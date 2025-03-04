8692805a5a67a3c87579f5c0197a1d32b3f1803b
Guilhem <73174073+GuilhemTrd@users.noreply.github.com>
2025-03-04 10:59:17 +0100

    Feature/48 front landing page (#55)
    * ✨feat : implement landing page with features and translations
    
    * 🔧fix : enhance landing page with separate register and login buttons, update translations
    
    * Fix tests
    
    ---------
    
    Co-authored-by: Hugo POIZAT <hugo.poizat@ynov.com>

--- src/app/component/landing/landing.component.ts
+++ src/app/component/landing/landing.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { TranslatePipe } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    TranslatePipe,
+    RouterLink
+  ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
-export class LandingComponent {
-}
+export class LandingComponent {}
